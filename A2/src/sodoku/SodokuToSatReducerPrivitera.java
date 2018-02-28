8a3984aa72f21d1664c0bffdea7d14980c5aaf5a
Caleb Privitera <32499212+cpriv41@users.noreply.github.com>
2018-02-27 20:40:35 -0500

    Fixed atLeastOneInBox
    Still have a doubt about atMostOneInBox

--- A2/src/sodoku/SodokuToSatReducerPrivitera.java
+++ A2/src/sodoku/SodokuToSatReducerPrivitera.java
@@ -3,8 +3,7 @@ package sodoku;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.PrintWriter;
-import java.io.Writer;
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
@@ -12,13 +11,17 @@ public class SodokuToSatReducerPrivitera {
 
 	private File cnfOut;
 	private PrintWriter writer;
-	private PrintWriter pw;
 	public int boxWidth;
 	public int boxHeight;
 	public int numCells;
 	boolean[] variables;
+	LinkedList<String> clauses = new LinkedList<>();
 	// boolean[] boxes;
 	SodokuBoardPrivitera sodokuBoard;
+	int rowcount, mrowcount;
+	int colcount, mcolcount;
+	int boxcount, mboxcount;
+	int cellcount, mcellcount;
 	// final private static int[] DEFAULT_VALUE = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
 	public SodokuToSatReducerPrivitera(File inputFile) throws Exception {
@@ -45,7 +48,6 @@ public class SodokuToSatReducerPrivitera {
 
 			while (board.findInLine(pat) != null)
 				// skip comments
-				// once at 'c' read line
 				board.nextLine();
 
 			// get board dimensions
@@ -55,7 +57,7 @@ public class SodokuToSatReducerPrivitera {
 			sodokuBoard = new SodokuBoardPrivitera(boxWidth, boxHeight);
 
 			// process data from input file
-			for (int cell = 1; cell <= sodokuBoard.getNumberOfCells() - 1; cell++) {
+			for (int cell = 1; cell <= sodokuBoard.getNumberOfCells(); cell++) {
 				sodokuBoard.setCellValue(cell - 1, board.nextInt());
 
 			}
@@ -76,92 +78,61 @@ public class SodokuToSatReducerPrivitera {
 	public void initializeBoard() {
 
 		for (int i = 0; i < sodokuBoard.getNumberOfCells(); i++) {
-			if (sodokuBoard.getCellValue(i) != 0)
-				variables[getVariableIndex(sodokuBoard.getCellRow(i), sodokuBoard.getCellColumn(i),
-						sodokuBoard.getCellValue(i))] = true;
+			if (sodokuBoard.getCellValue(i) != 0) {
+				variables[sodokuBoard.getCellValue(i)] = true;
+			}
 		}
 		return;
 
 	}
-	
-	static void reduceBoard(){
+
+	public void reduceBoard() {
+		TimerPrivitera timer = new TimerPrivitera();
+
+		System.out.println("Processing input file: ");
+		timer.start();
 		reducer();
+		timer.stop();
+		System.out.println("Time elapsed: " + timer.getDuration() + "ms");
+		System.out.flush();
 	}
 
 	public void reducer() {
 		try {
-			createOutput("output");
-		
-		printFirstLine();
-		for (int i = 0; i < sodokuBoard.getBoardSize(); i++) {
-			for (int k = 1; k <= sodokuBoard.getBoardSize(); k++) {
-				atLeastOneInRow(i, k);
-				atMostOneInRow(i, k);
+
+			for (int i = 0; i < sodokuBoard.getBoardSize(); i++) {
+				for (int k = 1; k <= sodokuBoard.getBoardSize(); k++) {
+					atLeastOneInRow(i, k);
+					atMostOneInRow(i, k);
+				}
 			}
-		}
-		for (int j = 0; j < sodokuBoard.getBoardSize(); j++) {
-			for (int k = 1; k <= sodokuBoard.getBoardSize(); k++) {
-				atLeastOneInColumn(j, k);
-				atMostOneInColumn(j, k);
-			}
-		}
-		for (int i = 0; i < sodokuBoard.getBoardSize(); i++) {
-			for (int k = 1; k <= sodokuBoard.getBoardSize(); k++) {
-				atLeastOneInBox(i, k);
-				atMostOneInBox(i, k);
-			}
-		}
-		for (int i = 0; i < sodokuBoard.getBoardSize(); i++) {
+			//clauses.add("EndRows\n");
 			for (int j = 0; j < sodokuBoard.getBoardSize(); j++) {
-				atLeastOneInCell(i, j);
-				atMostOneInCell(i, j);
-
+				for (int k = 1; k <= sodokuBoard.getBoardSize(); k++) {
+					atLeastOneInColumn(j, k);
+					atMostOneInColumn(j, k);
+				}
 			}
-
-		}
-		writer.flush();
-		writer.close();
+			//clauses.add("EndCols\n");
+			for (int i = 0; i < sodokuBoard.getBoardSize(); i++) {
+				for (int k = 1; k <= sodokuBoard.getBoardSize(); k++) {
+					atLeastOneInBox(i, k);
+					atMostOneInBox(i, k);
+				}
+			}
+			//clauses.add("EndBox\n");
+			for (int i = 0; i < sodokuBoard.getBoardSize(); i++) {
+				for (int j = 0; j < sodokuBoard.getBoardSize(); j++) {
+					atLeastOneInCell(i, j);
+					atMostOneInCell(i, j);
+				}
+			}
+			createOutput("output");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
-	//
-	// public void initialzeBoxStatus() {
-	// boolean[] boxStatus = new boolean[sodokuBoard.getBoardSize()];
-	// // Check all boxes on board
-	// for (int i = 0; i < sodokuBoard.getBoardSize(); i++) {
-	// // Assume that the box has all required digits
-	// boxes[i] = true;
-	// // Check every digit in the box
-	// for (int j = 0; j < sodokuBoard.getBoxHeight(); j++) {
-	// for (int k = 0; k < sodokuBoard.getBoxWidth(); k++) {
-	// // Tick the appropriate indicator for every non-zero value
-	// if (getBoxValue(i, j, k) != 0)
-	// boxStatus[getBoxValue(i, j, k)] = true;
-	// }
-	// }
-	// // If any digits are missing, set the box status to false
-	// for (int x = 0; x < boxStatus.length; x++) {
-	// if (boxStatus[x] != true)
-	// boxes[i] = false;
-	// }
-	//
-	// // reset indicators for next box analysis
-	// Arrays.fill(boxStatus, false);
-	// }
-	// return;
-	// }
-	//
-	// public int getBoxValue(int boxNum, int row, int column) {
-	//
-	// int cellIndex = (boxNum/boxWidth * sodokuBoard.getBoardSize() *
-	// boxHeight) + (boxNum % boxWidth)*3 + (row * sodokuBoard.getBoardSize()) +
-	// column;
-	// return sodokuBoard.getCellValue(cellIndex);
-	// }
-	//
 
 	/**
 	 * 
@@ -174,113 +145,100 @@ public class SodokuToSatReducerPrivitera {
 	 * @return
 	 */
 	public int convert(int row, int column, int value) {
-
 		return (row * sodokuBoard.getBoardSize() * sodokuBoard.getBoardSize() + column * sodokuBoard.getBoardSize()
 				+ value);
-
 	}
 
-	private int getVariableIndex(int row, int column, int value) {
-
-		return (value - 1) + (sodokuBoard.getNumberOfCells()) + row * (sodokuBoard.getBoardSize() + column);
-	}
-
+	/**
+	 * 
+	 * @param row
+	 * @param value
+	 */
 	public void atLeastOneInRow(int row, int value) {
+		StringBuffer clause = new StringBuffer();
 
 		for (int col = 0; col < sodokuBoard.getBoardSize(); col++) {
-			//if (variables[getVariableIndex(row, col, value)] == true)
-				writer.print(Integer.toString(convert(row, col, value)) + " ");
-
+			clause.append(Integer.toString(convert(row, col, value)) + " ");
 		}
-		writer.println("0");
-
+		clause.append("0\n");
+		clauses.add(clause.toString());
+		rowcount++;
 	}
 
 	private void atMostOneInRow(int row, int value) {
-
 		for (int j = 0; j < sodokuBoard.getBoardSize(); j++) {
 			for (int k = j + 1; k < sodokuBoard.getBoardSize(); k++) {
-				// if (variables[getVariableIndex(j, k, value)] == true) {
-				writer.println(Integer.toString(-1 * convert(row, j, value)) + " "
-						+ Integer.toString(-1 * convert(row, k, value)) + " 0");
-
-				// }
+				clauses.add(Integer.toString(-1 * convert(row, j, value)) + " "
+						+ Integer.toString(-1 * convert(row, k, value)) + " 0\n");
+				mrowcount++;
 			}
 		}
-
 	}
 
 	public void atLeastOneInColumn(int column, int value) {
+		StringBuffer clause = new StringBuffer();
+
 		for (int row = 0; row < sodokuBoard.getBoardSize(); row++) {
-			//if (variables[getVariableIndex(row, column, value)] == true)
-				writer.print(Integer.toString(convert(row, column, value)) + " ");
-
+			clause.append(Integer.toString(convert(row, column, value)) + " ");
 		}
-		writer.println("0");
-
+		clause.append("0\n");
+		clauses.add(clause.toString());
+		colcount++;
 	}
 
 	public void atMostOneInColumn(int column, int value) {
-
 		for (int j = 0; j < sodokuBoard.getBoardSize(); j++) {
 			for (int k = j + 1; k < sodokuBoard.getBoardSize(); k++) {
-				writer.println(Integer.toString(-1 * convert(j, column, value)) + " "
-						+ Integer.toString(-1 * convert(k, column, value)) + " 0");
-
+				clauses.add(Integer.toString(-1 * convert(j, column, value)) + " "
+						+ Integer.toString(-1 * convert(k, column, value)) + " 0\n");
+				mcolcount++;
 			}
 		}
 	}
 
 	public void atLeastOneInBox(int box, int value) {
-
-		for (int row = 0; row < sodokuBoard.getBoardSize(); row++) {
+		StringBuffer clause = new StringBuffer();
+		for (int row = 0, cell = 0; row < sodokuBoard.getBoardSize(); row++) {
 			for (int column = row + 1; column < sodokuBoard.getBoardSize(); column++) {
-				if (sodokuBoard.getCellBox(value) != box)
+				if (sodokuBoard.getCellBox(cell++) != box)
 					continue;
-				//if (variables[getVariableIndex(row, column, value)] == true)
-					writer.print(Integer.toString(convert(row, column, value)) + " ");
-
+				clause.append(Integer.toString(convert(row, column, value)) + " ");
 			}
 		}
-		writer.println("0");
-
+		clause.append("0\n");
+		clauses.add(clause.toString());
+		boxcount++;
 	}
 
 	public void atMostOneInBox(int box, int value) {
-
 		for (int row = 0; row < sodokuBoard.getBoardSize(); row++) {
 			for (int column = row + 1; column < sodokuBoard.getBoardSize(); column++) {
-				if (sodokuBoard.getCellBox(value) != box)
-					continue;
-
-				writer.println(Integer.toString(-1 * convert(row, column, value)) + " "
-						+ Integer.toString(-1 * convert(row, column, value)) + " 0");
-
+				clauses.add(Integer.toString(-1 * convert(row, column, value)) + " "
+						+ Integer.toString(-1 * convert(row, column, value)) + " 0\n");
+				mboxcount++;
 			}
 		}
-
 	}
 
 	public void atLeastOneInCell(int row, int column) {
-
-		for (int k = 1; k < sodokuBoard.getBoardSize(); k++) {
-			// if (variables[getVariableIndex(row, column, k)] == true)
-			writer.print(Integer.toString(convert(row, column, k)) + " ");
-
+		StringBuffer clause = new StringBuffer();
+		for (int k = 1; k <= sodokuBoard.getBoardSize(); k++) {
+			clause.append(Integer.toString(convert(row, column, k)) + " ");
 		}
-		writer.println("0");
+		clause.append("0\n");
+		clauses.add(clause.toString());
+		cellcount++;
 
 	}
 
 	private void atMostOneInCell(int row, int column) {
-
-		for (int x = 0; x < sodokuBoard.getBoardSize(); x++) {
-			for (int y = x + 1; y < sodokuBoard.getBoardSize(); y++)
-				writer.println(Integer.toString(-1 * convert(row, column, x)) + " "
-						+ Integer.toString(-1 * convert(row, column, y)) + " 0");
-
+		for (int x = 1; x <= sodokuBoard.getBoardSize(); x++) {
+			for (int y = x + 1; y <= sodokuBoard.getBoardSize(); y++) {
+				clauses.add(Integer.toString(-1 * convert(row, column, x)) + " "
+						+ Integer.toString(-1 * convert(row, column, y)) + " 0\n");
+				mcellcount++;
+			}
 		}
-
 	}
 
 	private int numberOfVariables() {
@@ -288,8 +246,7 @@ public class SodokuToSatReducerPrivitera {
 	}
 
 	private int numberOfClauses() {
-		return sodokuBoard.getBoardSize() * sodokuBoard.getBoardSize() * sodokuBoard.getBoardSize()
-				* sodokuBoard.getNumberOfCells();
+		return clauses.size();
 	}
 
 	private void printFirstLine() {
@@ -298,12 +255,22 @@ public class SodokuToSatReducerPrivitera {
 
 	private void createOutput(String outFile) throws Exception {
 		cnfOut = new File(outFile + ".cnf");
-
+		System.out.println("Output " + numberOfClauses() + " clauses");
+		System.out.println("Row Clauses " + rowcount + "/" + mrowcount);
+		System.out.println("Col Clauses " + colcount + "/" + mcolcount);
+		System.out.println("Box Clauses " + boxcount + "/" + mboxcount);
+		System.out.println("Cell Clauses " + cellcount + "/" + mcellcount);
 		try {
 			writer = new PrintWriter(cnfOut);
+			printFirstLine();
+			for (String clause : clauses) {
+				writer.print(clause);
+			}
+			writer.flush();
+			writer.close();
 		} catch (Exception e) {
 			throw e;
-		
+
 		}
 	}
 
